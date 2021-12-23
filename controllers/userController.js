9dd93176ac02a526fab6a6bbf63832c0dd8fd13d
TauH <thaywood988@gmail.com>
2021-12-23 14:48:00 +1100

    user controllers changed to try catch, all user routes done

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,29 +1,71 @@
 const { Thought, User } = require("../models");
 
 module.exports = {
-  createUser(req, res) {
-    User.create(req.body)
-      .then((newUser) => res.json(newUser))
-      .catch((err) => res.status(500).json(err));
+  async createUser(req, res) {
+    try {
+      const newUser = await User.create(req.body);
+      res.status(200).json(newUser);
+    } catch (err) {
+      res.status(500).json(err);
+    }
   },
-  getUsers(req, res) {
-    User.find({}).then((users) =>
-      res.json(users).catch((err) => res.status(500).json(err))
-    );
+  async getUsers(req, res) {
+    try {
+      const allUsers = await User.find({});
+      res.status(200).json(allUsers);
+    } catch (err) {
+      res.status(500).json(err);
+    }
   },
-  getOneUser(req, res) {
-    User.find({ _id: req.params.userId }).then((users) =>
-      res.json(users).catch((err) => res.status(500).json(err))
-    );
+  async getOneUser(req, res) {
+    try {
+      const user = await User.find({ _id: req.params.userId });
+      res.status(200).json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
   },
-  changeUser(req, res) {
-    User.updateOne({ _id: req.params.userId }, { $set: req.body }).then(
-      (users) => res.json(users).catch((err) => res.status(500).json(err))
-    );
+  async changeUser(req, res) {
+    try {
+      const updatedUser = await User.updateOne(
+        { _id: req.params.userId },
+        { $set: req.body }
+      );
+      res.status(200).json(updatedUser);
+    } catch (err) {
+      res.status(500).json(err);
+    }
   },
-  removeUser(req, res) {
-    User.remove({ _id: req.params.userId }).then((users) =>
-      res.json(users).catch((err) => res.status(500).json(err))
-    );
+  async removeUser(req, res) {
+    try {
+      let userRemoved = await User.remove({ _id: req.params.userId });
+      res.status(200).json(userRemoved);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  async addFriend(req, res) {
+    try {
+      let user = await User.findOne({ _id: req.params.userId });
+      await user.friends.push(req.params.friendsId);
+      //if statement so cant have the same freidn twice
+      await user.save();
+
+      res.status(200).json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+  async removeFriend(req, res) {
+    try {
+      let user = await User.updateOne(
+        { _id: req.params.userId },
+        { $pull: { friends: req.params.friendsId } }
+      );
+
+      res.status(200).json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
   },
 };
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -6,12 +6,17 @@ const {
   getOneUser,
   changeUser,
   removeUser,
+  addFriend,
+  removeFriend,
 } = require("../../controllers/userController");
 
 router.route("/").post(createUser).get(getUsers);
 
 router.route("/:userId").get(getOneUser).put(changeUser).delete(removeUser);
 
-router.route("/:userId/friends/:friendsId").post().delete();
+router
+  .route("/:userId/friends/:friendsId")
+  .post(addFriend)
+  .delete(removeFriend);
 
 module.exports = router;
